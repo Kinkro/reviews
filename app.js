f24df4d15e973d68bd8f82693aac09791d4e9881
Alexandre <alexkinkriashvili@gmail.com>
2022-01-26 18:01:39 +0400

    Modified

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const reviews = [
       name: "susan smith",
       job: "web developer",
       img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+        "./images/person-0.jpg",
       text:
         "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
@@ -14,7 +14,7 @@ const reviews = [
       name: "anna johnson",
       job: "web designer",
       img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        "./images/person-1.jpg",
       text:
         "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
     },
@@ -23,7 +23,7 @@ const reviews = [
       name: "peter jones",
       job: "intern",
       img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+        "./images/person-2.jpg",
       text:
         "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
     },
@@ -32,9 +32,32 @@ const reviews = [
       name: "bill anderson",
       job: "the boss",
       img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        "./images/person-3.jpg",
       text:
         "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
     },
   ];
   
+//   select items
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+
+// set starting item
+
+let currentItem = 0;
+
+// load initial item
+
+window.addEventListener("DOMContentLoaded", function(){
+    const item = reviews[currentItem];
+    img.src = item.img;
+
+})
